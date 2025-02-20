8f8e589f0cfbfab158a1e8c19d471d316c8c19cb
Rishi-Trueigtech <rdubey@trueigtech.com>
2025-02-20 16:56:11 +0530

    Make three model User,Tour,Booking

---
+++ Travel_API/models/tour.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Tour extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      Tour.hasMany(models.Booking,{foreignKey:"tourId"});
+    }
+  }
+  Tour.init({
+    name: DataTypes.STRING,
+    duration: DataTypes.INTEGER,
+    difficulty: DataTypes.STRING,
+    price: DataTypes.FLOAT,
+    summary: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Tour',
+  });
+  return Tour;
+};
