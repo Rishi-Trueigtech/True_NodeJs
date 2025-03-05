acfae357534cac8a85f2145cfac073453031e914
Rishi-Trueigtech <rdubey@trueigtech.com>
2025-03-05 19:05:30 +0530

    Implemented Rooms in Socketio

--- Basic_Socketio/server.js
+++ Basic_Socketio/server.js
@@ -13,13 +13,20 @@ const io = new Server(server, {cors:{origin:"*"}});
 io.on("connection",(socket)=>{
     console.log("New User Connected:", socket.id);
 
-    socket.on("message", (msg)=>{
-        console.log("Message received:", msg);
-        io.emit("message", msg);
-    })
+    socket.on("joinRoom", (room)=>{
+        // console.log("Message received:", msg);
+        // io.emit("message", msg);
+        socket.join(room);
+        console.log(`User ${socket.id} joined room: ${room}`);
+        socket.to(room).emit("message",`User${socket.id} joined the chat.`);
+    });
+
+    socket.on("roomMessage",({room, message})=>{
+        io.to(room).emit("message", message);
+    });
 
     socket.on("disconnect", ()=> console.log("User disconnected:", socket.id));
 });
 
 
-server.listen(3000, ()=> console.log("Server running on http://localhost:3000"));
+server.listen(3000, ()=> console.log("Server running on http://"));
